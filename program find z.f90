51556e300970ca1c99c4efc6af2e95f970d2a715
nursultanadilbekov <nursultanadilbekov5@gmail.com>
2024-09-13 16:54:34 +0600

    machine epsilon, z and i

--- program find z.f90
+++ program find z.f90
@@ -1,17 +1,17 @@
-program find epsilon
+program find z
     implicit none
     integer :: n
     real(8) :: e
     n = 0
-    e = 1.0d0
+    e = 2.0d0
 
     do
         n = n + 1
         e = e / 10.0d0
         print*, 'Iteration ', n, ': e = ', e
 
-        if (1.0d0 + e == 1.0d0) exit
+        if (2.0d0 * e == e) exit
     end do
 
     print*, 'Number of iterations: ', n
-end program find epsilon
+end program epsilon_check
