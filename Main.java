9e8a38dc075e75d44286b8a6b663353ba16e1c07
Nursultan <152598465+nursultanadilbekov@users.noreply.github.com>
2024-09-27 12:16:41 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,27 +1,25 @@
 import java.util.Random;
-import java.util.Scanner;
 
 public class Main
 {
 	public static void main(String[] args) {
 
 		Random random = new Random();
-		Scanner scanner = new Scanner(System.in);
 
-		int a = 10;
-		int b = -10;
-		int c = a-b;
+		double a = 10;
+		double b = -10;
+		double c = a-b;
 		double x;
 		int i = 0;
-		double Sum = 0;
+		double S = 0;
 		do{
-			double r = random.nextInt(b - a + 1) + a;
+			double r = a + (b - a) * random.nextDouble();
 			x=r;
-			double function = x-2;
-			Sum=Sum+r*function;
+			double function = x*x-2;
+			S=S+r*function;
 			i++;
 			if (i==99){
-				System.out.println(Sum/i*c);
+				System.out.println(S/i*c);
 			}
 		}while(i<100);
 	}
